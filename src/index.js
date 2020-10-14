9bd0e7021784563c4e5fd37060be0d8803916a80
gabrieldss <gabriel-santosouza@hotmail.com>
2020-10-14 00:10:43 -0300

    adjust name component import

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import rootReducer from './reducers/root';
+import root from './reducers/root';
 import App from './App';
 
-const store = createStore(rootReducer)
+const store = createStore(root)
 
 ReactDOM.render(
 <Provider store={store}><App /></Provider>, document.getElementById('root'));
